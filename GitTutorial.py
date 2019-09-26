e8dd46715685783626f66cb382f919f914f3cc50
Joshua <mcbratjo@gmail.com>
2019-09-25 20:41:10 -0600

    This was modified with some comments explaining git

---
+++ GitTutorial.py
@@ -0,0 +1,22 @@
+
+# GIT TUTORIAL
+#
+#
+# 1. Navigate to the directory of the project you're working in:
+#      cd PROJECT_FOLDER
+# 2. A) Create a repository:
+#      git init
+#    B) Clone an existing repository from GitHub (This is the most common):
+#
+# 3. Check the changes made:
+#       git status
+# 4. Add the changes of a file to the staging area (or use -A for all files):
+#       git add FILENAME_WITH_EXTENSION
+# 5. After adding changes to the staging area, commit the changes (use -m "MESSAGE" for message):
+#       git commit -m "THIS IS MY COMMIT MESSAGE"
+#
+#
+#
+#
+#
+print("Hello World")
