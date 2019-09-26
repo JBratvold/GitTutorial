9453da66ca47e4836156583040ed6c67c720ffc6
Joshua <mcbratjo@gmail.com>
2019-09-25 20:44:07 -0600

    Spaced out and continued the Git Tutorial Comments

--- GitTutorial.py
+++ GitTutorial.py
@@ -3,19 +3,24 @@
 #
 #
 # 1. Navigate to the directory of the project you're working in:
-#      cd PROJECT_FOLDER
+#       cd PROJECT_FOLDER
+
 # 2. A) Create a repository:
-#      git init
+#       git init
 #    B) Clone an existing repository from GitHub (This is the most common):
-#
+#       git clone https://github.com/SOMEONES_NAME/SOMEONES_PROJECT
+
 # 3. Check the changes made:
 #       git status
+
 # 4. Add the changes of a file to the staging area (or use -A for all files):
 #       git add FILENAME_WITH_EXTENSION
+
 # 5. After adding changes to the staging area, commit the changes (use -m "MESSAGE" for message):
 #       git commit -m "THIS IS MY COMMIT MESSAGE"
-#
-#
+
+# 6. To get the most up to date copy from your local Git (NOT FROM GITHUB) use:
+#       git checkout -- .
 #
 #
 #
