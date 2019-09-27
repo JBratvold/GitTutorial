8c667d0f445c0c6a4cba2ac8c831a3ab562f1f4e
Joshua <mcbratjo@gmail.com>
2019-09-26 21:08:04 -0600

    Added some more to the GitHelp and modified a few (like #4)

--- GitTutorial.py
+++ GitTutorial.py
@@ -1,13 +1,24 @@
 
 # GIT TUTORIAL
 #
+# =========== SETUP ===============
+# git config --global user.name "USER NAME"
+# git config --global user.email "EMAIL@EMAIL.COM"
+# git config --list
+#
+
+# ======= GIT HELP =============
+# git help config
+# git config --help
+
+
 #
 # 1. Navigate to the directory of the project you're working in:
 #       cd PROJECT_FOLDER
 
-# 2. A) Create a repository:
+# 2. A) Create a repository on your local machine:
 #       git init
-#    B) Clone an existing repository from GitHub (This is the most common):
+#    B) Clone an existing repository remotely from GitHub (This is the most common):
 #       git clone https://github.com/SOMEONES_NAME/SOMEONES_PROJECT.git
 
 # 3. Check the changes made:
@@ -15,9 +26,13 @@
 
 # 4. Add the changes of a file to the staging area (or use -A for all files):
 #       git add FILENAME_WITH_EXTENSION
+#             OR [TO REMOVE/RESET]
+#       git reset FILENAME_WITH_EXTENSION
+#                   OR
+#       git reset
 
 # 5. After adding changes to the staging area, commit the changes (use -m "MESSAGE" for message):
-#       git commit -m "THIS IS MY COMMIT MESSAGE"
+#       git commit -m "THIS IS MY USEFUL COMMIT MESSAGE EXPLAINING WHAT YOU DID"
 
 # 6. To get the most up to date copy from the previous local commit (NOT FROM GITHUB) use:
 #       git checkout -- .
